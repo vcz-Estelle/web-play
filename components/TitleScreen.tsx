b98e1399d2c970162d802c9b53ed637b377c30b9
정현지 <blackit14@gmail.com>
2026-06-13 22:42:35 +0900

    💄 타이틀 + favicon 수정

--- components/TitleScreen.tsx
+++ components/TitleScreen.tsx
@@ -2,6 +2,7 @@
 export default function TitleScreen({ onStart }: { onStart: () => void }) {
   return (
     <div className='flex flex-col items-center gap-5 text-center max-w-md'>
+      <h1 className='text-3xl font-extrabold tracking-tight'>Rewind - *h3 *t* S*mM3*</h1>
       <p className='text-white/70 leading-relaxed'>
         소나기가 내려오면,
         <br /> 이건 잠시뿐일 거야.
